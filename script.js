4c25d3fcd930af9e812cb6415c9eb8afcc97b68c
Ananasaaa <kairyakn@gmail.com>
2024-05-26 23:46:48 +0300

    lesson_3.3

---
+++ script.js
@@ -0,0 +1,18 @@
+// Homework 3.3
+
+//first solution
+
+let num = 10369;
+
+num = num.toString().split('').join(' ');
+
+console.log(num);
+
+
+//second solution
+
+let numAlt = 10386;
+
+ console.log(`${numAlt - 10385} ${numAlt - 10386} ${numAlt - 10383} ${numAlt - 10378} ${numAlt - 10380}`);
+
+
